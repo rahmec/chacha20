40752e8e759633d91b6a1523ba866294e3fe517a
rahmec <rahmi.elmechri@gmail.com>
2025-02-11 00:07:37 +0100

    Almost done

--- src/chacha20.c
+++ src/chacha20.c
@@ -1,26 +1,55 @@
 #include <stdio.h> // I/O handling
 #include <stdint.h> // uint handling
-#include <stdlib.h> // memory handling
+#include <string.h> // memory handling
 
 #define ROTL(a,b) (((a) << (b)) | ((a) >> (32-(b))))
 
-const uint32_t C0 = 0x00000000;
-const uint32_t C1 = 0x00000000;
-const uint32_t C2 = 0x00000000;
-const uint32_t C3 = 0x00000000;
+const uint32_t C0 = 0x12341234;
+const uint32_t C1 = 0x56785678;
+const uint32_t C2 = 0x91011910;
+const uint32_t C3 = 0xaabbccdd;
 
 typedef uint32_t seed256[8];
 typedef uint32_t ctr64[2];
 typedef uint32_t nonce64[2];
 typedef uint32_t block512[16];
 
-uint32_t* pad_blocks(seed256 seed, ctr64 ctr, nonce64 nonce);
-void permute_block(block512 *block);
-void quarter_round(block512 *block, int a, int b, int c, int d);
+void pad_blocks(seed256 seed, ctr64 ctr, nonce64 nonce, block512 block);
+void permute_block(block512 block);
+void quarter_round(block512 block, int a, int b, int c, int d);
 
 int main()
 {
-    printf("Hello World %d\n", 1);
+    seed256 seed = {
+        0x11111111, 0x22222222, 0x33333333, 0x44444444,
+        0x55555555, 0x66666666, 0x77777777, 0x88888888,
+    };
+
+    ctr64 ctr = {
+        0x99999999, 0xaaaaaaaa
+    };
+
+    nonce64 nonce = {
+        0xbbbbbbbb, 0xcccccccc
+    };
+
+    block512 block;
+    pad_blocks(seed, ctr, nonce, block);
+
+    printf("Block: [");
+    for(int i=0; i<16; i++){
+        printf(" 0x%x ",block[i]);
+    }
+    printf("]\n");
+
+    permute_block(block);
+
+    printf("Permuted block: [");
+    for(int i=0; i<16; i++){
+        printf(" 0x%x ",block[i]);
+    }
+    printf("]\n");
+
     return 0;
 }
 
@@ -32,10 +61,9 @@ int main()
  *  seed: 256-bit seed (8 32-bit blocks s0,...,s7)
  *  ctr: 64-bit counter (2 32-bit blocks c0,c1)
  *  nonce: 64-bit nonce (2 32-bit blocks n0,n1)
- *
- *  returns: 512-bit block (16 32-bit blocks b0,...,b15)
+ *  block: 512-bit block (16 32-bit blocks b0...b15)
 */
-uint32_t* pad_blocks(seed256 seed, ctr64 ctr, nonce64 nonce)
+void pad_blocks(seed256 seed, ctr64 ctr, nonce64 nonce, block512 block)
 {
     uint32_t paddedBlock[16] = {
        C0,         C1,         C2,         C3, 
@@ -44,10 +72,7 @@ uint32_t* pad_blocks(seed256 seed, ctr64 ctr, nonce64 nonce)
        ctr[0],     ctr[1],     nonce[0],   nonce[1]
     };
 
-    uint32_t* block = malloc(16*sizeof(uint32_t));
-    block = paddedBlock;
-    
-    return block;
+    memcpy(block, paddedBlock, (sizeof (uint32_t))*16);
 }
 
 /*
@@ -57,7 +82,7 @@ uint32_t* pad_blocks(seed256 seed, ctr64 ctr, nonce64 nonce)
  *
  *  block: 512-bit block (16 32-bit blocks b0,...,b15)
 */
-void permute_block(block512* block)
+void permute_block(block512 block)
 {
     // Apply to four columns
     quarter_round(block, 0,4,8,12);
@@ -80,16 +105,14 @@ void permute_block(block512* block)
  *  block: 512-bit block (16 32-bit blocks b0,...,b15)
  *  a,b,c,d: block sub-blocks' indexes
 */
-void quarter_round(block512 *block, int a, int b, int c, int d)
+void quarter_round(uint32_t block[], int a, int b, int c, int d)
 {
-   uint32_t* x_a = *(block + a); 
-   uint32_t* x_b = *(block + b); 
-   uint32_t* x_c = *(block + c); 
-   uint32_t* x_d = *(block + d);
+   uint32_t* x_a = &block[a]; 
+   uint32_t* x_b = &block[b]; 
+   uint32_t* x_c = &block[c]; 
+   uint32_t* x_d = &block[d]; 
    *x_a += *x_b; *x_d ^= *x_a; ROTL(*x_d,16);
    *x_c += *x_d; *x_b ^= *x_c; ROTL(*x_b,12);
    *x_a += *x_b; *x_d ^= *x_a; ROTL(*x_d,8);
    *x_c += *x_d; *x_b ^= *x_c; ROTL(*x_b,7);
 }
-
-
